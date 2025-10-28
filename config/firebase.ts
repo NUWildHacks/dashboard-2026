8d6048871bcfd95813a1c86c3395a7177c4c512c
Larry Ling <larryling.main@gmail.com>
2025-10-27 19:17:34 -0500

    initialize firebase

---
+++ config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDiLAifKtkeqjX2rpuQGL_6uq2vav6oN3Y",
+  authDomain: "wildhacks-dashboard-2026-dev.firebaseapp.com",
+  projectId: "wildhacks-dashboard-2026-dev",
+  storageBucket: "wildhacks-dashboard-2026-dev.firebasestorage.app",
+  messagingSenderId: "849337008203",
+  appId: "1:849337008203:web:67da23a3b1613799290dce",
+  measurementId: "G-Y89CH7EV9G"
+})
+
+
