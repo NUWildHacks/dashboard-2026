00d2a70f0b91b1ec4bdef1db1a4f6a52e56f6b97
Larry Ling <larryling.main@gmail.com>
2025-11-24 14:20:35 -0600

    announcement dialog layout

--- app/dashboard/_components/_announcements/announcement-dialog.tsx
+++ app/dashboard/_components/_announcements/announcement-dialog.tsx
@@ -1,13 +1,20 @@
 "use client";
 
+import { Clock } from "lucide-react";
+import Link from "next/link";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { getSendTime } from "@/lib/time";
 
 import { UseAnnouncementDialogReturn } from "../../_hooks/use-announcement-dialog";
 
@@ -18,15 +25,50 @@ export default function AnnouncementDialog({ isOpen, setIsOpen, selectedAnnounce
     return null;
   }
 
-  const { id, category, title, body, author, audience, links } = selectedAnnouncement;
+  const { category, title, body, author, audience, links, created_at } = selectedAnnouncement;
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{body}</DialogDescription>
+          <DialogTitle asChild>
+            <div className="flex justify-center sm:justify-start items-center gap-4">
+              {title}
+              <Badge variant="secondary">{category}</Badge>
+            </div>
+          </DialogTitle>
+          <DialogDescription asChild>
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-center sm:justify-start items-center gap-x-4 gap-y-2">
+                <Badge variant="outline">{author}</Badge>
+                <span className="flex items-center gap-1">
+                  <Clock className="size-3" />
+                  {getSendTime(created_at)}
+                </span>
+              </div>
+              <p className="text-center sm:text-left">{body}</p>
+              {links.length !== 0 && (
+                <div>
+                  <p className="font-semibold">Attached Links</p>
+                  <ul>
+                    {links.map((link) => (
+                      <li key={link}>
+                        <Link href={link} className="block">
+                          {link}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Close</Button>
+          </DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
