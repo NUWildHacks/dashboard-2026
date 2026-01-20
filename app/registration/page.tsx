0da4c8158e9ef069209d43d2503bc437e07222ce
Silvia Fang <fang.silvia@gmail.com>
2026-01-20 10:50:07 -0600

    fix(next): apply security patch

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -25,9 +25,9 @@ const RegistrationPage = async () => {
   if (userDocSnapshot.exists) redirect(DASHBOARD_PATH);
 
   return (
-    <main className="flex-1 px-6 sm:px-12 py-6 flex flex-col justify-center items-center">
+    <main className="flex-1 px-6 sm:px-12 py-12 flex flex-col justify-center items-center">
       <div className="max-w-[650px]">
-        <div className="text-center space-y-5">
+        <div className="text-center space-y-12">
           <h2 className="text-4xl sm:text-5xl font-semibold">Hey there, future hacker! 👋</h2>
           <p>
             Fill out the registration form below and you&apos;ll be all set. We just need some basic info to get you
