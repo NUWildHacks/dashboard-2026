4f9cb461da8d2ccc2ae17f0761f45cabd679dfda
Larry Ling <larryling.main@gmail.com>
2026-02-04 00:27:25 -0800

    feat: add temporary empty project page

--- app/dashboard/project/page.tsx
+++ app/dashboard/project/page.tsx
@@ -1,6 +1,9 @@
+import { FolderGit2 } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { EditProjectForm, EmptyProject, TeamMembersList } from "@/app/dashboard/project/_components";
+import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
 import { DASHBOARD_PATH, DASHBOARD_PROJECT_PATH, LOGIN_PATH, PARTICIPANT } from "@/constants";
 import { getAuthenticatedUser } from "@/lib";
 import type { ParticipantUser } from "@/types";
@@ -13,19 +16,39 @@ const ProjectPage = async () => {
   const user = await getAuthenticatedUser(redirectPath);
   if (user.role !== PARTICIPANT) redirect(DASHBOARD_PATH);
 
-  const { id: userId, project_id } = user as ParticipantUser;
-  const project = await getProject(project_id);
-
-  if (!project) {
-    return <EmptyProject />;
-  }
-
   return (
-    <div className="flex-1 flex flex-col lg:flex-row lg:items-start gap-4">
-      <EditProjectForm project={project} />
-      <TeamMembersList userId={userId} {...project} />
+    <div className="flex-1 flex justify-center items-center">
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <FolderGit2 />
+          </EmptyMedia>
+          <EmptyTitle>Project management coming soon</EmptyTitle>
+          <EmptyDescription>
+            Check back soon for updates. If you have any questions, please contact us at{" "}
+            <Link href="mailto:wildhacks@northwestern.edu" className="underline underline-offset-4">
+              wildhacks@northwestern.edu
+            </Link>
+            .
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
     </div>
   );
+
+  // const { id: userId, project_id } = user as ParticipantUser;
+  // const project = await getProject(project_id);
+
+  // if (!project) {
+  //   return <EmptyProject />;
+  // }
+
+  // return (
+  //   <div className="flex-1 flex flex-col lg:flex-row lg:items-start gap-4">
+  //     <EditProjectForm project={project} />
+  //     <TeamMembersList userId={userId} {...project} />
+  //   </div>
+  // );
 };
 
 export default ProjectPage;
