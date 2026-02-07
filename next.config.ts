d1453821fe2414a74ebe33df46d693b167e2a586
Larry Ling <larryling.main@gmail.com>
2026-02-06 20:46:20 -0800

    feat: add csp

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,37 @@ import type { NextConfig } from "next";
 
 import { WILDHACKS_HOME } from "./constants/routes.constants";
 
+const isDev = process.env.APP_ENV !== "production";
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ""} https://*.firebaseapp.com https://apis.google.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self' data:;
+  connect-src 'self' https://*.firebaseapp.com https://*.googleapis.com https://github.com;
+  frame-src 'self' https://*.firebaseapp.com;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`.replace(/\s{2,}/g, " ").trim();
+
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
