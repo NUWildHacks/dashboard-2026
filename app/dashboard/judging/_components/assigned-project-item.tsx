fffb2c85993f55512191249f06c573f713c1957e
Larry Ling <larryling.main@gmail.com>
2026-03-08 01:40:24 -0800

    feat: add submitted badge

--- app/dashboard/judging/_components/assigned-project-item.tsx
+++ app/dashboard/judging/_components/assigned-project-item.tsx
@@ -14,7 +14,7 @@ type AssignedProjectItemProps = Pick<UseJudgingFormSheetReturn, "handleOpenJudgi
 };
 
 const AssignedProjectItem = ({ handleOpenJudgingForm, projectWithJudgingForm }: AssignedProjectItemProps) => {
-  const { name, track } = projectWithJudgingForm;
+  const { name, track, judging_form } = projectWithJudgingForm;
 
   return (
     <Item variant="outline">
@@ -24,6 +24,14 @@ const AssignedProjectItem = ({ handleOpenJudgingForm, projectWithJudgingForm }: 
         </ItemTitle>
         <ItemDescription className="flex flex-row items-center gap-2">
           <Badge variant="secondary">{track}</Badge>
+          {judging_form && (
+            <Badge
+              variant="outline"
+              className="border-none bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 focus-visible:outline-none dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 [a&]:hover:bg-green-600/5 dark:[a&]:hover:bg-green-400/5"
+            >
+              Submitted
+            </Badge>
+          )}
         </ItemDescription>
       </ItemContent>
       <ItemActions>
