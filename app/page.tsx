23c9dbcac2439188b482ddf72dfe38439ab23b37
Silvia Fang <fang.silvia@gmail.com>
2026-03-10 17:08:23 -0500

    fix: removed registration buttons and moved dashboard button as main CTA

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ import { Footer, Navbar } from "@/app/_components";
 import { getConfigDocSnapshot } from "@/lib";
 import type { WildHacksConfig } from "@/types";
 
+import Closed from "./_components/closed";
 import Completed from "./_components/completed";
-import Ongoing from "./_components/ongoing";
+// import Ongoing from "./_components/ongoing";
 
 const RootPage = async () => {
   const configDocSnapshot = await getConfigDocSnapshot();
@@ -22,7 +23,9 @@ const RootPage = async () => {
       <main className="flex-1 px-6 sm:px-12 flex flex-col justify-center items-center">
         <div className="max-w-[700px] text-center space-y-5">
           <Image src="/wildhacks-splash.svg" alt="Main Logo" width={700} height={260} loading="eager" />
-          {now < end_time && <Ongoing />}
+          {/* {now < end_time && <Ongoing />} */}
+          {/* patch: close registration completely */}
+          {now < end_time && <Closed />}
           {now >= end_time && <Completed />}
         </div>
       </main>
