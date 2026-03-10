f2b9fe810e443e88e48aa399bc36cf762eb646c2
Silvia Fang <fang.silvia@gmail.com>
2026-03-10 17:11:55 -0500

    fix: prettier

--- app/_components/closed.tsx
+++ app/_components/closed.tsx
@@ -10,9 +10,9 @@ const Registration = async () => {
     <>
       <h2 className="text-4xl sm:text-5xl font-semibold">Welcome to WildHacks 2026! 🚀</h2>
       <p>
-        Registrations for participants, judges, and mentors are now closed, but the excitement is just getting started. 
-        For those joining us for the weekend, get ready to build something amazing in 24 hours alongside hundreds 
-        of students in a weekend of innovation, collaboration, and creativity!
+        Registrations for participants, judges, and mentors are now closed, but the excitement is just getting started.
+        For those joining us for the weekend, get ready to build something amazing in 24 hours alongside hundreds of
+        students in a weekend of innovation, collaboration, and creativity!
       </p>
       <div className="flex justify-center items-center gap-2 flex-wrap">
         <Link href={DASHBOARD_PATH}>
