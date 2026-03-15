bff7f2e29db770bd9eb3648a87283deb16adf4e6
Silvia Fang <fang.silvia@gmail.com>
2026-03-14 22:06:27 -0500

    delete: judging form sheet

--- app/dashboard/judging/_components/index.ts
+++ app/dashboard/judging/_components/index.ts
@@ -1,4 +1,3 @@
-export { default as JudgingFormSheet } from "./judging-form-sheet";
 export { default as JudgingDisplay } from "./judging-display";
 export { default as AssignedProjectItem } from "./assigned-project-item";
 export { default as AssignedProjectGrid } from "./assigned-project-grid";
