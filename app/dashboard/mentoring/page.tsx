2ac4c2b2d3f097ce813c01ade9bb482fbde9fece
Larry Ling <larryling.main@gmail.com>
2026-04-02 18:21:45 -0500

    feat: add auth user check

--- app/dashboard/mentoring/page.tsx
+++ app/dashboard/mentoring/page.tsx
@@ -1,4 +1,14 @@
+import { redirect } from "next/navigation";
+
+import { DASHBOARD_MENTORING_PATH, DASHBOARD_PATH, JUDGE_AND_MENTOR, LOGIN_PATH } from "@/constants";
+import { getAuthenticatedUser } from "@/lib";
+
 const MentoringPage = async () => {
+  const redirectPath = `${LOGIN_PATH}?redirect=${encodeURIComponent(DASHBOARD_MENTORING_PATH)}`;
+
+  const user = await getAuthenticatedUser(redirectPath);
+  if (user.role !== JUDGE_AND_MENTOR) redirect(DASHBOARD_PATH);
+
   return <div>MentoringPage</div>;
 };
 
