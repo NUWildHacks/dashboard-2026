ac53f6b9a874f7a3e8575c0e93c0c01559b44281
Larry Ling <larryling.main@gmail.com>
2026-04-03 17:58:48 -0500

    feat: add modality expectation text

--- app/dashboard/mentoring/_components/mentoring-timeslot-radio-group.tsx
+++ app/dashboard/mentoring/_components/mentoring-timeslot-radio-group.tsx
@@ -13,10 +13,11 @@ import { useMentoringTimeslotRadioGroup } from "../_hooks";
 import { TIMESLOT_CONFIRMATION_DEADLINE } from "../constants";
 
 type MentoringTimeslotRadioGroupProps = {
+  modality: JudgeAndMentorUser["modality"];
   mentoring_timeslot: JudgeAndMentorUser["mentoring_timeslot"];
 };
 
-const MentoringTimeslotRadioGroup = ({ mentoring_timeslot }: MentoringTimeslotRadioGroupProps) => {
+const MentoringTimeslotRadioGroup = ({ modality, mentoring_timeslot }: MentoringTimeslotRadioGroupProps) => {
   const { selectedMentoringTimeslot, setSelectedMentoringTimeslot, onSubmit, isSubmitting } =
     useMentoringTimeslotRadioGroup(mentoring_timeslot);
 
@@ -27,9 +28,13 @@ const MentoringTimeslotRadioGroup = ({ mentoring_timeslot }: MentoringTimeslotRa
       <CardHeader>
         <CardTitle>Mentoring Timeslot</CardTitle>
         <CardDescription>
-          Select when you will be available for mentoring. Keep in mind that mentoring will happen on{" "}
-          <span className="font-bold underline underline-offset-4">Saturday, April 11th</span> and you must confirm a
-          timeslot by <span className="font-bold underline underline-offset-4">Wednesday, April 6th, 11:59 PM CT.</span>
+          <div className="space-y-1">
+            <p>You have been assigned to a mentoring timeslot. If you wish to change it, you can do so by selecting a different timeslot below.</p>
+            <p>
+              Keep in mind that mentoring will happen on <span className="font-bold underline underline-offset-4">Saturday, April 11th</span> and you must confirm a
+              timeslot by <span className="font-bold underline underline-offset-4">Wednesday, April 6th, 11:59 PM CT.</span>
+            </p>
+          </div>
         </CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
@@ -50,6 +55,8 @@ const MentoringTimeslotRadioGroup = ({ mentoring_timeslot }: MentoringTimeslotRa
             </FieldLabel>
           ))}
         </RadioGroup>
+        {modality === "In-Person" && <p className="text-sm text-muted-foreground">Because you have selected to be in-person, we expect you to be present at the venue during your assigned timeslot.</p>}
+        {modality === "Remote" && <p className="text-sm text-muted-foreground">Because you have selected to be remote, we expect you to be available via our Discord server during your assigned timeslot.</p>}
       </CardContent>
       <CardFooter className="flex justify-end items-center gap-4">
         {isTimeslotConfirmationDeadlinePassed && (
