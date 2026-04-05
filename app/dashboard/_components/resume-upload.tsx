0e395f92e787be5f1f4d8928dfce8d9844e02063
Larry Ling <larryling.main@gmail.com>
2026-04-05 17:38:32 -0500

    chore: create useResumeUpload custom hook

--- app/dashboard/_components/resume-upload.tsx
+++ app/dashboard/_components/resume-upload.tsx
@@ -1,82 +1,27 @@
 "use client";
 
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { Download, File, FileX, Loader2, Trash, Upload } from "lucide-react";
-import { ChangeEvent, useRef, useState } from "react";
-import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
 import { cn } from "@/lib";
 
-import { deleteResume } from "../_actions/delete-resume";
-import { uploadResume } from "../_actions/upload-resume";
+import { useResumeUpload } from "../_hooks";
 
 type ResumeUploadProps = {
   fileName?: string;
 };
 
 const ResumeUpload = ({ fileName }: ResumeUploadProps) => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const [isUploading, setIsUploading] = useState(false);
-
-  const handleOpenFileInput = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleUploadResume = async (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
-    setIsUploading(true);
-
-    try {
-      const result = await uploadResume(file);
-      const { success } = result;
-
-      if (!success) {
-        const { error } = result;
-        throw new Error(error);
-      }
-
-      toast.success("Resume uploaded successfully");
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
-      toast.error("Failed to upload resume", { description: errorMessage });
-    } finally {
-      setIsUploading(false);
-    }
-  };
-
-  const handleDeleteResume = async () => {
-    if (!fileName) return;
-
-    const result = await deleteResume();
-    const { success } = result;
-
-    if (!success) {
-      const { error } = result;
-      throw new Error(error);
-    }
-
-    toast.success("Resume deleted successfully");
-  };
-
-  const handleDownloadResume = async () => {
-    if (!fileName) return;
-
-    const storage = getStorage();
-    const fileRef = ref(storage, fileName);
-
-    const url = await getDownloadURL(fileRef);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = fileName;
-    a.click();
-  };
+  const {
+    fileInputRef,
+    isUploading,
+    handleOpenFileInput,
+    handleUploadResume,
+    handleDeleteResume,
+    handleDownloadResume,
+  } = useResumeUpload(fileName);
 
   return (
     <Card className="shadow-xs size-full">
@@ -96,7 +41,7 @@ const ResumeUpload = ({ fileName }: ResumeUploadProps) => {
         <Item variant="outline" className="w-full">
           <ItemMedia variant="icon">{fileName ? <File /> : <FileX />}</ItemMedia>
           <ItemContent>
-            <ItemTitle className={cn("text-sm text-muted-foreground", { "italic": !fileName })}>
+            <ItemTitle className={cn("text-sm text-muted-foreground", { italic: !fileName })}>
               {fileName ? fileName : "You currently have no resume uploaded."}
             </ItemTitle>
           </ItemContent>
