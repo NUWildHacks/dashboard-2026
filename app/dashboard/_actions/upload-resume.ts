5c9e1dd00f9f96d748010723f239afa1d3acb68f
Silvia Fang <fang.silvia@gmail.com>
2026-04-05 19:27:16 -0500

    fix: allow admin to upload and delete

--- app/dashboard/_actions/upload-resume.ts
+++ app/dashboard/_actions/upload-resume.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 import { revalidatePath } from "next/cache";
 
-import { DASHBOARD_PATH, LOGIN_PATH, PARTICIPANT, RESUMES_COLLECTION } from "@/constants";
+import { ADMIN, DASHBOARD_PATH, LOGIN_PATH, RESUMES_COLLECTION } from "@/constants";
 import { getAuthenticatedUser, requireRole } from "@/lib";
 import { ActionResult } from "@/types";
 
@@ -25,7 +25,9 @@ export const uploadResume = async (resume: File): Promise<ActionResult> => {
     const user = await getAuthenticatedUser(redirectPath);
     const { id, first_name, last_name } = user;
 
-    const roleError = requireRole(user, PARTICIPANT, "You are not authorized to upload a resume");
+    // change back to participant after testing
+    // const roleError = requireRole(user, PARTICIPANT, "You are not authorized to upload a resume");
+    const roleError = requireRole(user, ADMIN, "You are not authorized to upload a resume");
     if (roleError) return roleError;
 
     const bucket = storage.bucket();
