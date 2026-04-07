66a800ded556a9ad5de3bfee0e0ac37af06d697c
Larry Ling <larryling.main@gmail.com>
2026-04-07 14:55:44 -0500

    feat: allow users with Judge/Mentor roles to access judging round pages

--- app/dashboard/(judging)/judging-round-1/page.tsx
+++ app/dashboard/(judging)/judging-round-1/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-import { DASHBOARD_JUDGING_ROUND_1_PATH, DASHBOARD_PATH, JUDGE, LOGIN_PATH } from "@/constants";
+import { DASHBOARD_JUDGING_ROUND_1_PATH, DASHBOARD_PATH, JUDGE, JUDGE_AND_MENTOR, LOGIN_PATH } from "@/constants";
 import { getAuthenticatedUser } from "@/lib";
 
 import { JudgingDisplay } from "../_components";
@@ -10,7 +10,7 @@ const JudgingRound1Page = async () => {
   const redirectPath = `${LOGIN_PATH}?redirect=${encodeURIComponent(DASHBOARD_JUDGING_ROUND_1_PATH)}`;
 
   const user = await getAuthenticatedUser(redirectPath);
-  if (user.role !== JUDGE) redirect(DASHBOARD_PATH);
+  if (user.role !== JUDGE && user.role !== JUDGE_AND_MENTOR) redirect(DASHBOARD_PATH);
 
   const { id, modality, other_modality } = user;
 
