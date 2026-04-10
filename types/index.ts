b6274a45b3070a9557bc8c44e6ae5c38625157a6
didiermun <didiermunezer38@gmail.com>
2026-04-10 12:41:49 -0500

    chores(format): prettier formatting

--- types/index.ts
+++ types/index.ts
@@ -6,11 +6,7 @@ export type {
   GetEventCheckInsActionResponse,
   QRCodeScanPayload,
 } from "./check-in.types";
-export type {
-  CrowdFavoriteProject,
-  CrowdFavoriteTeamMember,
-  Vote,
-} from "./crowd-favorite.types";
+export type { CrowdFavoriteProject, CrowdFavoriteTeamMember, Vote } from "./crowd-favorite.types";
 export type { ErrorPageProps } from "./error-page.types";
 export type {
   AdminUser,
